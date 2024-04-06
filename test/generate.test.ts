cc252683be5a0768df7790e0e9f7398112a7d294
FSolM <fersolmarin@gmail.com>
2024-04-06 15:07:31 -0600

    Tests Updated

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,28 +1,28 @@
-import generator from "../src";
+import generate from "../src";
 
 test('expect empty options to be all lowercase values', () => {
-  const test = generator(10);
+  const test = generate(10);
 
   expect(/[a-z]/g.test(test)).toBe(true);
   expect(/[A-Z]/g.test(test)).not.toBe(true);
 });
 
 test('expect uppercase & lowercase values if options are passed saying so', () => {
-  const test = generator(10, { lowercaseLetters: true, uppercaseLetters: true });
+  const test = generate(10, { lowercaseLetters: true, uppercaseLetters: true });
 
   expect(/[A-z]/g.test(test)).toBe(true);
   expect(/[0-9]/g.test(test)).not.toBe(true);
 });
 
 test('expect all types of letters and numbers if options are passed saying so', () => {
-  const test = generator(10, { lowercaseLetters: true, uppercaseLetters: true, numbers: true });
+  const test = generate(10, { lowercaseLetters: true, uppercaseLetters: true, numbers: true });
 
   expect(/[A-z0-9]/g.test(test)).toBe(true);
   expect(/!@#$%^&*()_\-\+=/g.test(test)).not.toBe(true);
 });
 
 test('expect all types of values if all options are enabled', () => {
-  const test = generator(10, {
+  const test = generate(10, {
     lowercaseLetters: true,
     uppercaseLetters: true,
     numbers: true,
